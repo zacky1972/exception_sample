e43b579a97e33d7dc3cdb50240774a05d7cb8a8a
Susumu Yamazaki <zacky1972@gmail.com>
2023-05-25 09:39:04 +0900

    remove hello

--- lib/exception_sample.ex
+++ lib/exception_sample.ex
@@ -2,17 +2,4 @@ defmodule ExceptionSample do
   @moduledoc """
   Documentation for `ExceptionSample`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExceptionSample.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
